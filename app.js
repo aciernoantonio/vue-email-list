84f443ad26356945a43982affeddff327de1878d
aciernoantonio <aciernoantonio.int@gmail.com>
2022-04-29 15:13:46 +0200

    log of api random mail object

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const app = new Vue({
     el: `#app`,
 
-    data:{},
+    data:{
+    },
 
     methods:{},
 
     mounted() {
 
         axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+        .then(response => {
 
+            console.log(this);
+            console.log(response);
+
+            console.log(response.data.response);
+
+        })
     }
 })
